a0ffad855ab3a07e80466053463d9d643c829be1
Mateuzao <mateuzao88@gmail.com>
2021-05-06 18:08:27 -0300

    first commit, add code for project

---
+++ src/test/java/com/springserver/soap/webservices/soapspring/SoapSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springserver.soap.webservices.soapspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
